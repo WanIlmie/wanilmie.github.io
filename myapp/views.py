4ba4c34610ffb4f5026db7fd9f9db09676ec64b7
Wan Ilmie <101396318+WanIlmie@users.noreply.github.com>
2022-12-30 18:26:07 +0800

    change name

--- myapp/views.py
+++ myapp/views.py
@@ -105,7 +105,7 @@ def home(request):
 
     # Create context from query object
     context = {
-        'name': 'Wan Ilmie',
+        'name': 'Wan M Ilmie',
         'title': 'portfolio',
         'card_title': c.title,
         'card_text': c.text,
